f8aa693579104b83e12fab686a4a813f33b13d7a
hamzaJaral <hamzajarral62@gmail.com>
2024-03-13 01:47:01 +0500

    setting up project

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # Loading env variables
-import os
 
-from fastapi import FastAPI
-from app.routers import root
-
-from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routers import root
 
 load_dotenv()
 
